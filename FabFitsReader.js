4bda0246376580333aa4e08015794b431c9eab41
Fabrizio Giordano <fabriziogiordano77@gmail.com>
2020-09-30 15:47:39 +0200

    improved min/max support. Improved GUI. Improved API with the possibility to change pixels scale, color map and scale function programmatically

--- FabFitsReader.js
+++ FabFitsReader.js
@@ -9,6 +9,12 @@
  * @author Fabrizio Giordano <fabriziogiordano77@gmail.com>
  * 
  * 
+ * FITS standard keywords document:
+ * https://heasarc.gsfc.nasa.gov/docs/fcg/standard_dict.html
+ * 
+ * FITS standard:
+ * http://archive.stsci.edu/fits/fits_standard/
+ * 
  * 
  * BITPIX definition from https://archive.stsci.edu/fits/fits_standard/node39.html
  * 8	Character or unsigned binary integer
@@ -33,13 +39,18 @@ class FabFitsReader {
 		
 //		colorMap = [];
 		colorMap;
-		PVMIN = "undefined";
-		PVMAX = "undefined";
+		PVMIN = "NaN";
+		PVMAX = "NaN";
+		PVMIN_orig = "NaN";
+		PVMAX_orig = "NaN";
+		BLANK_pv;
 		physicalValues = [];
 		img;
 		tfPhysicalValues;
 		tFunction;
-	
+		data;
+		headerOffset;
+		
 	/**
 	 * @param url: FITS HTTP URL
 	 * @param in_colorMap: array in the form of one from ColorMap.js
@@ -51,11 +62,14 @@ class FabFitsReader {
 		var self = this;
 		this.colorMap = in_colorMap;
 		this.tFunction = in_tFunction;
-		if (typeof pvMin == 'number'){
-			this.PVMIN = pvMin;
+		if (typeof pvMin == 'number' && pvMin != null){
+//			console.log("min IS a number"+pvMin);
+//			this.PVMIN = pvMin;
+			this.PVMIN_orig = pvMin;
 		}
-		if (typeof pvMax == 'number'){
-			this.PVMAX = pvMax;
+		if (typeof pvMax == 'number' && pvMax != null){
+//			this.PVMAX = pvMax;
+			this.PVMAX_orig = pvMax;
 		}
 		
 		var xhr = new XMLHttpRequest();
@@ -75,7 +89,11 @@ class FabFitsReader {
 	
 	processFits (data) {
 		var headerOffset = this.readHeader(data);
-		return this.readPayload(data, headerOffset);
+		
+		this.data = data;
+		this.headerOffset =  headerOffset;
+		
+		return this.readPayload();
 	}
 	
 	readHeader (fitsFile) {
@@ -142,6 +160,14 @@ class FabFitsReader {
 			this.BSCALE = header.BSCALE; 
 		}
 		
+		if (isNaN(this.PVMIN_orig) && header.DATAMIN !== "undefined"){
+			this.PVMIN_orig = header.DATAMIN;
+		}
+		
+		if (isNaN(this.PVMAX_orig) && header.DATAMAX !== "undefined"){
+			this.PVMAX_orig = header.DATAMAX;
+		}
+		
 		this.physicalValues = new Array(this.width * this.height);
 		
 		console.log("width "+this.width+", height "+this.height);
@@ -150,14 +176,15 @@ class FabFitsReader {
 		return offset;
 	}
 	
+	firstRun = true;
 	
-	readPayload (fitsFile, headerOffset){		
-		length = fitsFile.length;
-    	var i = headerOffset;
+	readPayload (in_min, in_max){		
+		let length = this.data.length;
+    	var i = this.headerOffset;
     	var p = 0;
 		var x;
-		var min = 'NaN';
-		var max = 'NaN';
+//		var min = 'NaN';
+//		var max = 'NaN';
 		var c=document.createElement('canvas');
     	c.width = this.width;
         c.height = this.height;
@@ -167,54 +194,114 @@ class FabFitsReader {
     	// NON FUNZIONAAAAA
 //    	min = max = val = this.getLongAt(fitsFile, i, true); //IEEE float32 is always big-endian
 //    	max =0;
-    	var maxindex;
-    	let checkMin = isNaN(this.PVMIN) ? true : false;
-    	let checkMax = isNaN(this.PVMAX) ? true : false;
+//    	var maxindex;
+//    	let checkMin = isNaN(this.PVMIN) ? true : false;
+//    	let checkMax = isNaN(this.PVMAX) ? true : false;
+    	
     	
     	if (this.BZERO == undefined || this.BSCALE == undefined){
     		this.BZERO = 0;
     		this.BSCALE = 1;
 		}
     	
-    	let BLANK_pv = this.BZERO + this.BSCALE * this.BLANK;
+    	this.BLANK_pv = this.BZERO + this.BSCALE * this.BLANK;
     	
     	
     	var p = 0;
     	
-    	let p_val = BLANK_pv;
+//    	let p_val = this.BLANK_pv;
+    	
+    	
+    	let  min2bChecked = false;
+    	let  max2bChecked = false;
+    	let p_val;
+    	
+    	if (isNaN(this.PVMIN_orig)){
+    		min2bChecked = true;
+    	}
+    	if (isNaN(this.PVMAX_orig)){
+    		max2bChecked = true;
+    	}
+    	
+    	if (in_min != null){
+    		this.PVMIN = in_min;
+    	}else if (isNaN(this.PVMIN_orig)){
+    		p_val = this.getPhysicalNumber(i);
+    		this.PVMIN = p_val;
+    	}else {
+    		this.PVMIN = this.PVMIN_orig;
+    	}
+    	
+    	if (in_max != null){
+    		this.PVMAX = in_max;
+    	}else if (isNaN(this.PVMAX_orig)){
+    		p_val = this.getPhysicalNumber(i);
+    		this.PVMAX = p_val;
+    	}else {
+    		this.PVMAX = this.PVMAX_orig;
+    	}
+    	
+    	
+    	console.log("this.PVMIN "+this.PVMIN);
+		console.log("this.PVMAX "+this.PVMAX);
+    	
     	
 		while (i < length){
-			
+//			p_val = this.getPhysicalNumber(i);
 			if (this.BITPIX == 16){ // 16-bit 2's complement binary integer
-				
-				val = this.parse16bit2sComplement(fitsFile, i, true); //IEEE 754 half precision (float16)
-			
+				val = this.parse16bit2sComplement(this.data, i, true); //IEEE 754 half precision (float16)
 			}else if (this.BITPIX == -32){	// 32 bit single precision 
-			
-				val = this.parse32bitSinglePrecisionFloatingPoint (fitsFile, i, true); //IEEE 754 float32 is always big-endian	
+				val = this.parse32bitSinglePrecisionFloatingPoint (this.data, i, true); //IEEE 754 float32 is always big-endian	
 				if (val != 0){
 					val = (1.0+((val&0x007fffff)/0x0800000)) * Math.pow(2,((val&0x7f800000)>>23) - 127); // long to float conversion
 //					val = (1.0+((val&0x007fffff)/(val&0x0800000)>>31)) * Math.pow(2,((val&0x7f800000)>>23) - 127);
 				}
 			}
 			
+			
+			
 			// STSCI standard: physical_value = BZERO + BSCALE * array_value;
 			p_val = this.BZERO + this.BSCALE * val;
-
-			// this could be checked against the array blanck value val !== this.BLANK
-			if( checkMin && p_val !== BLANK_pv && (p_val < min || isNaN(min) ) ) {  
-				this.PVMIN = p_val;
-			}
-			if( checkMax && p_val !== BLANK_pv && (p_val < max || isNaN(max) ) ) {
-				this.PVMAX = p_val;
-			}
 			
-			this.physicalValues[p++] = p_val;
+			// this could be checked against the array blanck value val !== this.BLANK
+			if( min2bChecked && p_val < this.PVMIN ) {
+				this.PVMIN = p_val;
+				console.log(this.PVMIN);
+			}
+
+			if(max2bChecked && p_val > this.PVMAX) {
+				this.PVMAX = p_val;
+				console.log(this.PVMAX);
+			}
+
+			
+			
+			if( p_val !== this.BLANK_pv && p_val >= this.PVMIN && p_val <= this.PVMAX ) {
+				this.physicalValues[p++] = p_val;
+//				i += Math.abs(this.BITPIX/8);
+			}
+			else{
+				this.physicalValues[p++] = "NaN";	
+			} 
+
 			i += Math.abs(this.BITPIX/8);
 		}
-
+		console.log("this.PVMIN "+this.PVMIN);
+		console.log("this.PVMAX "+this.PVMAX);
+		
+		if (this.firstRun){
+			this.firstRun = false;		
+			if (isNaN(this.PVMIN_orig)){
+				this.PVMIN_orig = this.PVMIN;
+			}
+			if (isNaN(this.PVMAX_orig)){
+				this.PVMAX_orig = this.PVMAX;
+			}
+		}
+		
+		
 		this.tfPhysicalValues = this.physicalValues;
-		this.img = this.applyTransferFunction(this.tFunction);
+		this.img = this.applyScaleFunction(this.tFunction);
 		
 //		this.img = this.applyColorMap(this.colorMap);
     
@@ -222,13 +309,44 @@ class FabFitsReader {
 
 	}
 	
+	
+	
+	
+	get min(){
+		return this.PVMIN_orig;
+	}
+	
+	get max(){
+		return this.PVMAX_orig;
+	}
+	
+	set min(in_min){
+		this.PVMIN = in_min;
+	}
+	
+	set max(in_max){
+		this.PVMAX = in_max;
+	}
+	
 	get img(){
 		return this.img;
 	}
 	
+	getPhysicalNumber(idx){
+		let val;
+    	if (this.BITPIX == 16){ // 16-bit 2's complement binary integer
+    		val = this.parse16bit2sComplement(this.data, idx, true); //IEEE 754 half precision (float16)
+		}else if (this.BITPIX == -32){	// 32 bit single precision 
+			val = this.parse32bitSinglePrecisionFloatingPoint (this.data, idx, true); //IEEE 754 float32 is always big-endian	
+			if (val != 0){
+				val = (1.0+((val&0x007fffff)/0x0800000)) * Math.pow(2,((val&0x7f800000)>>23) - 127); // long to float conversion
+			}
+		}
+    	let p_val = this.BZERO + this.BSCALE * val;
+    	return p_val;
+	}
 	
-	
-	applyTransferFunction(tFunc){
+	applyScaleFunction(tFunc){
 		var self = this;
 		this.tfPhysicalValues = [];
 		if (tFunc == 'log'){
@@ -346,6 +464,15 @@ class FabFitsReader {
 			idx = -idx;
 		}
 		
+		if (v <= this.BLANK_pv ){
+//			console.log(v);
+			return {
+				r:0,
+				g:0,
+				b:0
+			};
+		}
+		
 		if (colorMapName == 'grayscale'){
 			
 			return {
@@ -358,41 +485,11 @@ class FabFitsReader {
 				r:colorMap.r[idx],
 				g:colorMap.g[idx],
 				b:colorMap.b[idx]
-			};	
-		}
-		
-		
-		
-	}
-
-	colorImage2 (v,min,max){
-
-		if (v<0) v = -v;
-	//	var colormap_idx = Math.round(( (v-min) / (max-min)) * 255);
-		var colormap_idx = ( (v-min) / (max-min)) * 256;
-		var idx = Math.round(colormap_idx);
-		if (idx<0){
-			idx = -idx;
-		}
-		
-		if (this.colorMap.name == 'grayscale'){
-			
-			return {
-				r:idx,
-				g:idx,
-				b:idx
 			};
-		}else{
-			return {
-				r:this.colorMap.r[idx],
-				g:this.colorMap.g[idx],
-				b:this.colorMap.b[idx]
-			};	
 		}
-		
-		
-		
+
 	}
+
 
 	trim (s) {
 		s = s.replace(/(^\s*)|(\s*$)/gi,"");
